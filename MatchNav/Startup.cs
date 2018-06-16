580b41b490348be7b4b3fac888411c316f8d84e1
kieranbrinks13 <kieranbrinkley18@gmail.com>
2018-06-16 13:46:32 +0100

    Add project files.

---
+++ MatchNav/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MatchNav.Startup))]
+namespace MatchNav
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
